9f2dd45e482512191f345048309a405985cdb356
vosmi <vosmi@DESKTOP-QA6OEV9>
2020-10-27 12:51:53 +0100

    Přidejte soubory projektu.

---
+++ ToDo_App/Helper/DbHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Database;
+using Android.Database.Sqlite;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ToDo_App.Helper
+{
+    public class DbHelper : SQLiteOpenHelper
+    {
+        private static String DB_Name = "KalushiToDoApp";
+        private static int DB_Ver = 1;
+        public static String DB_Table = "Task";
+        public static String DB_Column = "TaskName";
+
+        public DbHelper(Context context): base(context, DB_Name, null, DB_Ver) { }
+        public override void OnCreate(SQLiteDatabase db)
+        {
+            string query = $"CREATE TABLE {DbHelper.DB_Table}(ID INTEGER PRIMARY KEY AUTOINCREMENT, {DbHelper.DB_Column} TEXT NOT NULL)";
+            db.ExecSQL(query);
+        }
+
+        public override void OnUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+        {
+            string query = $"DELETE TABLE IF EXISTS{DB_Table}";
+            db.ExecSQL(query);
+            OnCreate(db);
+        }
+
+        public void InsertNewTask(String task)
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            ContentValues values = new ContentValues();
+            values.Put(DB_Column, task);
+            db.InsertWithOnConflict(DB_Table, null, values, Android.Database.Sqlite.Conflict.Replace);
+            db.Close();
+        }
+
+        public void DeleteTask(String task)
+        {
+            SQLiteDatabase db = this.WritableDatabase;
+            db.Delete(DB_Table, DB_Column + "=?", new String[] { task });
+            db.Close();
+        }
+
+        public List<string> getTaskList()
+        {
+            List<string> taskList = new List<string>();
+            SQLiteDatabase db = this.ReadableDatabase;
+            ICursor cursor = db.Query(DB_Table,new string[] { DB_Column }, null, null, null, null, null);
+            while (cursor.MoveToNext())
+            {
+                int index = cursor.GetColumnIndex(DB_Column);
+                taskList.Add(cursor.GetString(index));
+            }
+            return taskList;
+        }
+    }
+}
---
+++ ToDo_App/MainActivity.cs
@@ -0,0 +1,88 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
+using System;
+using ToDo_App.Helper;
+using System.Collections.Generic;
+
+namespace ToDo_App
+{
+    [Activity(
+        MainLauncher = true,
+        Label = "todo listík pro pana Olžbuta", 
+        Theme = "@style/AppTheme"
+    )]
+    public class MainActivity : AppCompatActivity
+    {
+        CustomAdapter losAdapteros;
+        ListView seznamTasku;
+        EditText edittexticek;
+        DbHelper databazicka;
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_item, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId){
+                case Resource.Id.action_add:
+                    edittexticek = new EditText(this);
+                    Android.Support.V7.App.AlertDialog dialog = new Android.Support.V7.App.AlertDialog.Builder(this)
+                        .SetTitle("přidat úkol")
+                        .SetMessage("napiste co chcete udelat:")
+                        .SetPositiveButton("pridat", OkAction)
+                        .SetView(edittexticek)
+                        .SetNegativeButton("zrusit", CancelAction)
+                        .Create();
+                    dialog.Show();
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void OkAction(object sender, DialogClickEventArgs e)
+        {
+            string task = edittexticek.Text;
+            databazicka.InsertNewTask(task);
+            LoadTaskList();
+        }
+
+        private void CancelAction(object sender, DialogClickEventArgs e)
+        {
+        }
+
+        public void LoadTaskList()
+        {
+            List<string> taskList = databazicka.getTaskList();
+            losAdapteros = new CustomAdapter(this, taskList, databazicka);
+            seznamTasku.Adapter = losAdapteros;
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            databazicka = new DbHelper(this);
+            seznamTasku = FindViewById<ListView>(Resource.Id.listTask);
+
+            LoadTaskList();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
